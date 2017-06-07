ef13c0c6062422301e83625ee0bea14f67b71cb8
SergeofBIBEK <SergeofBIBEK@gmail.com>
2017-06-07 01:09:15 -0400

    more stuff

--- main.js
+++ main.js
@@ -450,3 +450,19 @@ function saveCustomPath()
     externalAssetPath = path;
     loadCustomData();
 }
+
+var createCORSRequest = function(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // Most browsers.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // IE8 & IE9
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+};
--- share.js
+++ share.js
@@ -182,3 +182,19 @@ function loadCustomData()
         DataManager.loadDataFile("$dataAnimations", externalAssetPath + "/data/Animations.json");
     }
 }
+
+var createCORSRequest = function(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // Most browsers.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // IE8 & IE9
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+};
